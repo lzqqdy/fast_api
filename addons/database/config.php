f4ca85d1a58ceee91370dcfaebd6019d67f1b2a3
lzqqdy <986499810@qq.com>
2019-08-29 14:27:19 +0800

    插件更新

---
+++ addons/database/config.php
@@ -0,0 +1,49 @@
+<?php
+
+return array (
+  0 => 
+  array (
+    'name' => 'backupDir',
+    'title' => '备份存放目录',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => '../data/',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '备份目录,请使用相对目录',
+    'ok' => '',
+    'extend' => '',
+  ),
+  1 => 
+  array (
+    'name' => 'backupIgnoreTables',
+    'title' => '备份忽略的表',
+    'type' => 'string',
+    'content' => 
+    array (
+    ),
+    'value' => 'fa_admin_log',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '忽略备份的表,多个表以,进行分隔',
+    'ok' => '',
+    'extend' => '',
+  ),
+  2 => 
+  array (
+    'name' => '__tips__',
+    'title' => '温馨提示',
+    'type' => '',
+    'content' => 
+    array (
+    ),
+    'value' => '请做好数据库离线备份工作，建议此插件仅用于开发阶段，项目正式上线建议卸载此插件',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+);
