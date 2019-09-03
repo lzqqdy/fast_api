36fcb5dba993aabe9c62d3307eef00d6e7f6014a
lzqqdy <986499810@qq.com>
2019-09-03 17:49:30 +0800

    引入dd()调试方法，放弃修改halt避免冲突

--- application/api/common.php
+++ application/api/common.php
@@ -1,1 +1,16 @@
 <?php
+
+use think\exception\HttpResponseException;
+use think\Response;
+
+if (!function_exists('dd')) {
+    /**
+     * 自定义调试变量并且中断输出
+     * @param mixed $var 调试变量或者信息
+     */
+    function dd(...$var)
+    {
+        dump(...$var);
+        throw new HttpResponseException(new Response);
+    }
+}
