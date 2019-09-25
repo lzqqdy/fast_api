6ed1b69b589fad4a79fcf3d3834d67360bd3277a
lzqqdy <986499810@qq.com>
2019-09-25 15:07:20 +0800

    调整api返回格式

--- application/common/controller/Api.php
+++ application/common/controller/Api.php
@@ -164,16 +164,17 @@ class Api
      */
     protected function loadlang($name)
     {
-        Lang::load(APP_PATH . $this->request->module() . '/lang/' . $this->request->langset() . '/' . str_replace('.', '/', $name) . '.php');
+        Lang::load(APP_PATH . $this->request->module() . '/lang/' . $this->request->langset() . '/' . str_replace('.',
+                '/', $name) . '.php');
     }
 
     /**
      * 操作成功返回的数据
-     * @param string $msg    提示信息
-     * @param mixed  $data   要返回的数据
-     * @param int    $code   错误码，默认为1
-     * @param string $type   输出类型
-     * @param array  $header 发送的 Header 信息
+     * @param string $msg 提示信息
+     * @param mixed $data 要返回的数据
+     * @param int $code 错误码，默认为1
+     * @param string $type 输出类型
+     * @param array $header 发送的 Header 信息
      */
     protected function success($msg = '', $data = null, $code = 1, $type = null, array $header = [])
     {
@@ -182,11 +183,11 @@ class Api
 
     /**
      * 操作失败返回的数据
-     * @param string $msg    提示信息
-     * @param mixed  $data   要返回的数据
-     * @param int    $code   错误码，默认为0
-     * @param string $type   输出类型
-     * @param array  $header 发送的 Header 信息
+     * @param string $msg 提示信息
+     * @param mixed $data 要返回的数据
+     * @param int $code 错误码，默认为0
+     * @param string $type 输出类型
+     * @param array $header 发送的 Header 信息
      */
     protected function error($msg = '', $data = null, $code = 0, $type = null, array $header = [])
     {
@@ -196,21 +197,21 @@ class Api
     /**
      * 返回封装后的 API 数据到客户端
      * @access protected
-     * @param mixed  $msg    提示信息
-     * @param mixed  $data   要返回的数据
-     * @param int    $code   错误码，默认为0
-     * @param string $type   输出类型，支持json/xml/jsonp
-     * @param array  $header 发送的 Header 信息
+     * @param mixed $msg 提示信息
+     * @param mixed $data 要返回的数据
+     * @param int $code 错误码，默认为0
+     * @param string $type 输出类型，支持json/xml/jsonp
+     * @param array $header 发送的 Header 信息
      * @return void
      * @throws HttpResponseException
      */
     protected function result($msg, $data = null, $code = 0, $type = null, array $header = [])
     {
         $result = [
-            'code' => $code,
-            'msg'  => $msg,
-            'time' => Request::instance()->server('REQUEST_TIME'),
-            'data' => $data,
+            'code'   => $code,
+            'msg'    => $msg,
+            'time'   => Request::instance()->server('REQUEST_TIME'),
+            'result' => $data,
         ];
         // 如果未设置类型则自动判断
         $type = $type ? $type : ($this->request->param(config('var_jsonp_handler')) ? 'jsonp' : $this->responseType);
@@ -229,8 +230,8 @@ class Api
     /**
      * 前置操作
      * @access protected
-     * @param  string $method  前置操作方法名
-     * @param  array  $options 调用参数 ['only'=>[...]] 或者 ['except'=>[...]]
+     * @param  string $method 前置操作方法名
+     * @param  array $options 调用参数 ['only'=>[...]] 或者 ['except'=>[...]]
      * @return void
      */
     protected function beforeAction($method, $options = [])
@@ -272,11 +273,11 @@ class Api
     /**
      * 验证数据
      * @access protected
-     * @param  array        $data     数据
+     * @param  array $data 数据
      * @param  string|array $validate 验证器名或者验证规则数组
-     * @param  array        $message  提示信息
-     * @param  bool         $batch    是否批量验证
-     * @param  mixed        $callback 回调方法（闭包）
+     * @param  array $message 提示信息
+     * @param  bool $batch 是否批量验证
+     * @param  mixed $callback 回调方法（闭包）
      * @return array|string|true
      * @throws ValidateException
      */
