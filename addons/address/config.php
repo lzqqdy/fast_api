48c81765941b29d08323ff7d2ed444fa28fff135
lzqqdy <986499810@qq.com>
2019-10-12 11:51:21 +0800

    插件更新

--- addons/address/config.php
+++ addons/address/config.php
@@ -1,125 +1,142 @@
 <?php
 
-return array(
-    array(
-        'name'    => 'maptype',
-        'title'   => '默认地图类型',
-        'type'    => 'radio',
-        'content' =>
-            array(
-                'baidu'   => '百度地图',
-                'amap'    => '高德地图',
-                'tencent' => '腾讯地图',
-            ),
-        'value'   => 'baidu',
-        'rule'    => 'required',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => '',
+return array (
+  0 => 
+  array (
+    'name' => 'maptype',
+    'title' => '默认地图类型',
+    'type' => 'radio',
+    'content' => 
+    array (
+      'baidu' => '百度地图',
+      'amap' => '高德地图',
+      'tencent' => '腾讯地图',
     ),
-    array(
-        'name'    => 'location',
-        'title'   => '默认检索城市',
-        'type'    => 'string',
-        'content' =>
-            array(),
-        'value'   => '北京',
-        'rule'    => 'required',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => '',
+    'value' => 'baidu',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  1 => 
+  array (
+    'name' => 'location',
+    'title' => '默认检索城市',
+    'type' => 'string',
+    'content' => 
+    array (
     ),
-    array(
-        'name'    => 'zoom',
-        'title'   => '默认缩放级别',
-        'type'    => 'string',
-        'content' =>
-            array(),
-        'value'   => '12',
-        'rule'    => 'required',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => '',
+    'value' => '北京',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  2 => 
+  array (
+    'name' => 'zoom',
+    'title' => '默认缩放级别',
+    'type' => 'string',
+    'content' => 
+    array (
     ),
-    array(
-        'name'    => 'lat',
-        'title'   => '默认Lat',
-        'type'    => 'string',
-        'content' =>
-            array(),
-        'value'   => '39.919990',
-        'rule'    => 'required',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => '',
+    'value' => '12',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  3 => 
+  array (
+    'name' => 'lat',
+    'title' => '默认Lat',
+    'type' => 'string',
+    'content' => 
+    array (
     ),
-    array(
-        'name'    => 'lng',
-        'title'   => '默认Lng',
-        'type'    => 'string',
-        'content' =>
-            array(),
-        'value'   => '116.456270',
-        'rule'    => 'required',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => '',
+    'value' => '39.919990',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  4 => 
+  array (
+    'name' => 'lng',
+    'title' => '默认Lng',
+    'type' => 'string',
+    'content' => 
+    array (
     ),
-    array(
-        'name'    => 'baidukey',
-        'title'   => '百度地图KEY',
-        'type'    => 'string',
-        'content' =>
-            array(),
-        'value'   => 'hAeMFHmpyHa2ZjaCH9VVridl',
-        'rule'    => 'required',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => '',
+    'value' => '116.456270',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  5 => 
+  array (
+    'name' => 'baidukey',
+    'title' => '百度地图KEY',
+    'type' => 'string',
+    'content' => 
+    array (
     ),
-    array(
-        'name'    => 'amapkey',
-        'title'   => '高德地图KEY',
-        'type'    => 'string',
-        'content' =>
-            array(),
-        'value'   => '608d75903d29ad471362f8c58c550daf',
-        'rule'    => 'required',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => '',
+    'value' => 'hAeMFHmpyHa2ZjaCH9VVridl',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  6 => 
+  array (
+    'name' => 'amapkey',
+    'title' => '高德地图KEY',
+    'type' => 'string',
+    'content' => 
+    array (
     ),
-    array(
-        'name'    => 'tencentkey',
-        'title'   => '腾讯地图KEY',
-        'type'    => 'string',
-        'content' =>
-            array(),
-        'value'   => '608d75903d29ad471362f8c58c550daf',
-        'rule'    => 'required',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => '',
+    'value' => '608d75903d29ad471362f8c58c550daf',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  7 => 
+  array (
+    'name' => 'tencentkey',
+    'title' => '腾讯地图KEY',
+    'type' => 'string',
+    'content' => 
+    array (
     ),
-    array(
-        'name'    => '__tips__',
-        'title'   => '温馨提示',
-        'type'    => '',
-        'content' =>
-            array(),
-        'value'   => '请先申请对应地图的Key，配置后再使用',
-        'rule'    => '',
-        'msg'     => '',
-        'tip'     => '',
-        'ok'      => '',
-        'extend'  => 'alert-danger-light',
+    'value' => '608d75903d29ad471362f8c58c550daf',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  8 => 
+  array (
+    'name' => '__tips__',
+    'title' => '温馨提示',
+    'type' => '',
+    'content' => 
+    array (
     ),
+    'value' => '请先申请对应地图的Key，配置后再使用',
+    'rule' => '',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => 'alert-danger-light',
+  ),
 );
